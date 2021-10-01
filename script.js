1ec5c207f5159936483ae32133e941b89196b18b
Avin008 <32406268+Avin008@users.noreply.github.com>
2021-10-01 08:22:08 +0530

    updated checkInputFieldsFunc

--- script.js
+++ script.js
@@ -28,22 +28,25 @@ function fillNotes() {
 }
 
 checkBtn.addEventListener("click", function () {
-  if (!cashGiven.value || Number(cashGiven.value) < 1) {
-    displayMsg.innerText = "Enter valid Cash Given Amount";
-  } else {
-    if (Number(cashGiven.value) === Number(billAmount.value)) {
-      displayMsg.innerText = "no change should be returned";
+  if (cashGiven.value && billAmount.value) {
+    if (Number(cashGiven.value) < 1 || Number(billAmount.value) < 1) {
+      displayMsg.innerText =
+        "Enter Valid Amount, Amount Entered cannot be less than 1";
+      noNotes();
+    } else if (Number(cashGiven.value) === Number(billAmount.value)) {
+      displayMsg.innerText = "no change to be returned";
+      noNotes();
+    } else if (Number(cashGiven.value) < Number(billAmount.value)) {
+      displayMsg.innerText = "Cash Given cannot be less than Bill Amount";
+      noNotes();
     } else {
-      if (Number(cashGiven.value) > Number(billAmount.value)) {
-        let cashToBeReturned =
-          parseInt(cashGiven.value) - parseInt(billAmount.value);
-        displayMsg.innerText = `cash to be returned ${cashToBeReturned}`;
-        calculateChange(cashToBeReturned);
-      } else {
-        displayMsg.innerText =
-          "Cash Given Amount cannot be less than Bill Amount";
-      }
+      let cashToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
+      displayMsg.innerText = `cash to be returned ${cashToBeReturned}`;
+      calculateChange(cashToBeReturned);
     }
+  } else {
+    displayMsg.innerText = "Input fields cannot be empty";
+    noNotes();
   }
 });
 
@@ -68,5 +71,11 @@ function calculateChange(change) {
     let numNotes = Math.trunc(change / notes[x]);
     change %= notes[x];
     noOfNotes[x].innerText = numNotes;
+  }
+}
+
+function noNotes() {
+  for (let x = 0; x < noOfNotes.length; x++) {
+    noOfNotes[x].innerText = "0";
   }
 }
